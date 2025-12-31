f17a83937b8935b3637f3a5550c3e07e574081ac
Jetross <jetrossneri07@gmail.com>
2026-01-01 01:01:58 +0800

    feat: add background change on the theme toggle is hovered'

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Navbar } from "@/components/navbar";
 
 export default function Home() {
   return (
-    <div className="min-h-screen bg-background text-foreground gap-4 transition-colors duration-600">
+    <div className="min-h-screen bg-background text-foreground gap-4 transition-colors duration-300">
       <Navbar />
     </div>
   );
