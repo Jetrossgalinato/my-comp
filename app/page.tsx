40073ab13bdabe1ea2642e5c8c2a18712b98ea4e
Jetross <jetrossneri07@gmail.com>
2026-01-01 21:03:47 +0800

    feat: add header to the home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import { Navbar } from "@/components/navbar";
+import { Header } from "@/components/header";
 
 export default function Home() {
   return (
     <div className="min-h-screen bg-background text-foreground gap-4 transition-colors duration-300">
       <Navbar />
+      <Header />
     </div>
   );
 }
