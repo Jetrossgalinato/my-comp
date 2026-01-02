f46d65b89d7f39982724d000941d9e3fcac69324
Jetross <jetrossneri07@gmail.com>
2026-01-02 10:21:30 +0800

    feat: add view components button on the header

--- components/header.tsx
+++ components/header.tsx
@@ -4,15 +4,18 @@ export function Header() {
       <h1 className="text-5xl font-semibold">
         Professional Web Design, Simplified
       </h1>
-      <p className="text-xl font-normal mt-4 text-center max-w-2xl">
+      <p className="text-lg font-normal mt-5 text-center max-w-2xl">
         Explore our library of premium, responsive layouts. Customize with ease
         and launch professional projects without writing any CSS.
       </p>
 
       {/* Button Container */}
-      <div className="flex flex-row gap-4 mt-8">
-        <button className="bg-foreground text-background px-3 py-1 rounded-lg font-medium hover:bg-foreground/90 transition-colors duration-300">
+      <div className="flex flex-row gap-4 mt-5 text-sm">
+        <button className="bg-foreground text-background px-3 py-1 rounded-lg font-medium hover:bg-foreground/90 cursor-pointer transition-colors duration-300">
           Browse Layouts
+        </button>
+        <button className="bg-background text-foreground border border-gray-200 px-3 py-1 rounded-lg font-medium hover:bg-background/90 shadow-sm cursor-pointer transition-colors duration-300">
+          View Components
         </button>
       </div>
     </div>
