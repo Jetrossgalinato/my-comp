d34f2cf202dfe8024f1672cd226e4a8483176adb
Jetross <jetrossneri07@gmail.com>
2026-01-24 02:43:48 +0800

    feat: apply the typography component on the landing page

--- components/header.tsx
+++ components/header.tsx
@@ -1,24 +1,27 @@
 import { Search } from "lucide-react";
+import { TypographyH1, TypographyLead, TypographySmall } from "./typography";
 
 export function Header() {
   return (
     <div className="flex flex-col items-center justify-center px-4 h-100 w-full">
-      <h1 className="text-5xl font-semibold">
+      <TypographyH1 className="text-center">
         Professional Web Design, Simplified
-      </h1>
-      <p className="text-lg font-normal mt-6 text-center max-w-2xl">
+      </TypographyH1>
+      <TypographyLead className="mt-6 text-center max-w-2xl">
         Explore our library of premium, responsive layouts. Customize with ease
         and launch professional projects without writing any CSS.
-      </p>
+      </TypographyLead>
 
       {/* Button Container */}
-      <div className="flex flex-row gap-4 mt-6 text-sm">
-        <button className="bg-foreground text-background px-3 py-1 rounded-md font-medium hover:bg-foreground/90 cursor-pointer transition-colors duration-300">
-          <Search className="inline-block text-background/70 w-4 h-4 mb-1 mr-2" />
-          Browse Layouts
+      <div className="flex flex-row gap-4 mt-6">
+        <button className="bg-foreground text-background px-3 py-1 rounded-md hover:bg-foreground/90 cursor-pointer transition-colors duration-300">
+          <TypographySmall className="flex items-center">
+            <Search className="inline-block text-background/70 w-4 h-4 mr-2" />
+            Browse Layouts
+          </TypographySmall>
         </button>
-        <button className="text-foreground px-3 py-1 rounded-md font-medium hover:bg-foreground/5 cursor-pointer transition-colors duration-300">
-          View Components
+        <button className="text-foreground px-3 py-1 rounded-md hover:bg-foreground/5 cursor-pointer transition-colors duration-300">
+          <TypographySmall>View Components</TypographySmall>
         </button>
       </div>
     </div>
