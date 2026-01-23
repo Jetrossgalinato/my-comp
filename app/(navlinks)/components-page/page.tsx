45f43cc8e971124bc5c15d55a4780d8ccd2c0bdc
Jetross <jetrossneri07@gmail.com>
2026-01-24 03:05:08 +0800

    feat: import navbar on components page

--- app/(navlinks)/components-page/page.tsx
+++ app/(navlinks)/components-page/page.tsx
@@ -1,7 +1,8 @@
+import { Navbar } from "@/components/navbar";
 export default function ComponentsPage() {
   return (
-    <div className="min-h-screen bg-background text-foreground gap-4 transition-colors duration-300 flex items-center justify-center">
-      <h1 className="text-3xl font-bold">Components Page</h1>
+    <div className="min-h-screen bg-background text-foreground gap-4 transition-colors duration-300">
+      <Navbar />
     </div>
   );
 }
