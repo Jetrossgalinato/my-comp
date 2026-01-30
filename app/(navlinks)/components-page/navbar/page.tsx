406fa34d53a522830fe37b75613eb086ab2ea343
Jetross <jetrossneri07@gmail.com>
2026-01-30 10:20:49 +0800

    feat: add navbar page

---
+++ app/(navlinks)/components-page/navbar/page.tsx
@@ -0,0 +1,24 @@
+import { Navbar } from "@/components/navbar";
+import { Sidebar } from "../components/sidebar";
+import { TypographyH2, TypographyMuted } from "@/components/typography";
+export default function NavbarPage() {
+  return (
+    <div className="relative flex flex-col min-h-screen bg-background text-foreground transition-colors duration-300">
+      <Navbar />
+      <div className="flex flex-1 items-start">
+        <Sidebar />
+
+        <main className="flex-1 w-full ml-48">
+          <div className="container mx-auto px-4 py-16 max-w-3xl">
+            <div className="mb-10 space-y-4">
+              <TypographyH2>Navbar</TypographyH2>
+              <TypographyMuted className="text-base">
+                Minimalist and responsive navbar component
+              </TypographyMuted>
+            </div>
+          </div>
+        </main>
+      </div>
+    </div>
+  );
+}
